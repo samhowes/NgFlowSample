20dc61ae4bdbfc744b07a2036511074b5a76e5ef
Sam Howes <showes06@gmail.com>
2014-10-04 20:17:11 -0400

    Removed the unused HomeController

--- NgFlowSample/Global.asax.cs
+++ NgFlowSample/Global.asax.cs
@@ -16,7 +16,6 @@ namespace NgFlowSample
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
