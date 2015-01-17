1c72484883c06270d38dc98701b8cd8a3a411594
Sam Howes <showes06@gmail.com>
2015-01-17 04:14:10 -0500

    Added Explanatory comments to MultipartFormDataStreamProvider #2

--- NgFlowSample/Services/FlowMultipartFormDataStreamProvider.cs
+++ NgFlowSample/Services/FlowMultipartFormDataStreamProvider.cs
@@ -12,24 +12,30 @@ namespace NgFlowSample.Services
     public class FlowMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
     {
         public FlowMetaData MetaData;
+
         public readonly string Filename;
+
         public static Dictionary<string, FileStream> FlowFileStreams { get; set; }
 
         public FlowMultipartFormDataStreamProvider(string path): base(path)
         {}
 
+        /// <summary>
+        /// Process the form values that were submitted to construct a file name.
+        /// Make sure all form items come before the actual file data payload.
+        /// </summary>
         public override string GetLocalFileName(HttpContentHeaders headers)
         {
-
-            /*HttpContent requestContent = Request.Content;
-            string jsonContent = requestContent.ReadAsStringAsync().Result;
-            CONTACT contact = JsonConvert.DeserializeObject<CONTACT>(jsonContent);*/
             var flowMetaDictionary = new Dictionary<string, string>();
             foreach (var item in this.Contents)
             {
                 var param = item.Headers.ContentDisposition.Parameters.FirstOrDefault();
                 string key = param.Value.Trim('\"');
                 string value = item.ReadAsStringAsync().Result;
+
+                // Possible way to parse as JSON:
+                //MyObject myObj = JsonConvert.DeserializeObject<MyObject>(value);
+
                 flowMetaDictionary[key] = value;
             }
 
@@ -40,13 +46,28 @@ namespace NgFlowSample.Services
             return MetaData.FlowFilename;
         }
 
+        /// <summary>
+        /// <para>This method assumes that all form segments come before the actual file data.</para>
+        /// <para>Otherwise, GetLocalFileName will fail.</para>
+        /// </summary>
         public override Stream GetStream(HttpContent parent, HttpContentHeaders headers)
         {
             if (headers.ContentDisposition.FileName == null)
             {
                 return base.GetStream(parent, headers);
             }
-            var flowFileName = GetLocalFileName(headers);
+
+            string flowFileName = null;
+            try
+            {
+                flowFileName = GetLocalFileName(headers);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Flow chunk information was not properly transmitted before the chunk payload.", ex);
+            }
+
+            
 
             FileStream flowFileStream;
 
@@ -61,8 +82,13 @@ namespace NgFlowSample.Services
             return flowFileStream;
         }
 
+        /// <summary>
+        /// <para>The default implementation does what GetLocalFileName() does in here.</para>
+        /// <para>Make sure to override it so it doesn't try to do those things a second time.</para>
+        /// </summary>
         public override async Task ExecutePostProcessingAsync()
         {
+            // Nothing to do, we already processed the information in GetFileName
         }
     }
     
